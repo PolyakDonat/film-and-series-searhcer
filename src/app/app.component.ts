908b2bcc7eed4dae17e3eef7cc0a1ddc32b89169
PolyakDonatColdvalley <donat.polyak@coldvalley.io>
2020-12-07 18:50:47 +0100

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'film-and-series-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'film-and-series-searcher';
+}
