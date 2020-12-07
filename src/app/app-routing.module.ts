c433bb945e8258b14a58c66b52661dee8c8ba5ed
PolyakDonatColdvalley <donat.polyak@coldvalley.io>
2020-12-07 19:25:24 +0100

    Ui beállítások

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { OpeningComponent } from './pages/opening/opening.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: OpeningComponent },
+  { path: '', pathMatch: 'full', redirectTo: 'login' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { OpeningComponent } from './pages/opening/opening.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OpeningComponent
   ],
   imports: [
     BrowserModule,
